ef28f9ac67ea39109aaa675051a71dd4473a7e76
Ranjan Shrestha <ranjan@sharingapples.com>
2016-05-24 13:21:24 +0545

    definitions completed

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 module.exports = {
   TYPE_CALLBACK: 'Callback',
+
   TYPE_REMOTE_HANDLER: 'RemoteHandler',
+
   TYPE_REMOTE_INSTANCE: 'RemoteInstance',
 
+  /**
+   * Get the name of the event for the given namespace and name
+   * @param  {string} namespace     The namespace name (can be empty)
+   * @param  {string} name          The name of the method
+   * @return {string}               The event name to be used in the client as well
+   *                                as server
+   */
   eventName: function (namespace, name) {
     return namespace + '/' + name;
   },
 
+  /**
+   * Get the name of the event for the callback with the given id
+   * @param  {string} namespace The namespace name (can be empty)
+   * @param  {number} id        A unique id for the callback identification
+   * @return {string}           The event name to be used in the client as well
+   *                            as server
+   */
   eventCallbackName: function (namespace, id) {
     return namespace + '/' + 'cb' + '/' + id;
   },
